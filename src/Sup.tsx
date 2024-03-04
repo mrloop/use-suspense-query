b4be5cc7725294b306eb507faa7013b1863564fa
mrloop <ewan@mrloop.com>
2024-03-04 20:53:47 +0000

    Better name and group different demos

--- src/Sup.tsx
+++ src/Sup.tsx
@@ -1,15 +1,18 @@
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { Suspense } from 'react'
+import secondsSinceStart from "./timeSinceStart"
 
 const timeout = function (ms: number) {
   return new Promise(r => setTimeout(r, ms))
 }
 
-const queryFn = async function({ name, ms }: {name: string, ms: number }) {
-  console.log(`fetching: ${name}`);
-  await timeout(ms);
-  console.log(`fetched: ${name} (${ms}ms)`);
-  return `loaded ${name}`
+const query = function({ name, ms }: {name: string, ms: number }) {
+  return async function queryFn() {
+    console.log(`fetching: ${name} at ${secondsSinceStart()}s`);
+    await timeout(ms);
+    console.log(`fetched: ${name} (${ms}ms) at ${secondsSinceStart()}s`);
+    return `Loaded ${name} at ${secondsSinceStart()}s`
+  }
 }
 
 export default function Sup({ children, name, ms = 2000 }: { children?: any, name: string, ms?: number }) {
@@ -18,13 +21,15 @@ export default function Sup({ children, name, ms = 2000 }: { children?: any, nam
 
   const { data } = useSuspenseQuery({
      queryKey: [name],
-     queryFn: () => queryFn({name, ms}),
+     queryFn: query({name, ms}),
+     staleTime: Infinity,
+     refetchOnWindowFocus: false,
   })
 
   return (
     <div>
      {data}
-     <Suspense fallback={<div>{name} loading children...</div>}>
+     <Suspense fallback={<div className="loading">{name} loading children...</div>}>
        {children}
      </Suspense>
     </div>
