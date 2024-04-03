baa3291d1a4f679a754654961c38a003ae2d2ed2
mrloop <ewan@mrloop.com>
2024-04-03 07:28:30 +0100

    Better naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react'
 import './App.css'
-import Sup from './Sup'
+import DelayedQuery from './DelayedQuery'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -9,38 +9,38 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <section>
+        <section className="section">
           <h2>Nested</h2>
           <Suspense fallback={<div className="loading">Loading Nested...</div>}>
-            <Sup name={'Nested A'}>
-              <Sup name={'Nested B'}>
-                <Sup name={'Nested C'}><div>Nested Contents</div></Sup>
-              </Sup>
-            </Sup>
+            <DelayedQuery name={'Nested A'}>
+              <DelayedQuery name={'Nested B'}>
+                <DelayedQuery name={'Nested C'}><div>Nested Contents</div></DelayedQuery>
+              </DelayedQuery>
+            </DelayedQuery>
           </Suspense>
         </section>
 
-        <section>
+        <section className="section">
           <h2>Parallel</h2>
           <Suspense fallback={<div className="loading">Loading Parallel...</div>}>
-            <Sup name={'Parallel A'} ms={1000}/>
-            <Sup name={'Parallel B'} ms={2000}/>
-            <Sup name={'Parallel C'} ms={1000}/>
+            <DelayedQuery name={'Parallel A'} ms={1000}/>
+            <DelayedQuery name={'Parallel B'} ms={2000}/>
+            <DelayedQuery name={'Parallel C'} ms={1000}/>
           </Suspense>
         </section>
 
-        <section>
+        <section className="section">
           <h2>Cached</h2>
           <h3>Fast load first</h3>
           <Suspense fallback={<div className="loading">Loading Cached...</div>}>
-            <Sup name={'Cached A'} ms={4000}><Sup name={'Cached A'} ms={100000}></Sup></Sup>
-            <Sup name={'Cached A'} ms={10000}></Sup>
+            <DelayedQuery name={'Cached A'} ms={4000}><DelayedQuery name={'Cached A'} ms={100000}/></DelayedQuery>
+            <DelayedQuery name={'Cached A'} ms={10000}></DelayedQuery>
           </Suspense>
 
           <h3>Slow load first</h3>
           <Suspense fallback={<div className="loading">Loading Cached...</div>}>
-            <Sup name={'Cached B'} ms={10000}></Sup>
-            <Sup name={'Cached B'} ms={4000}><Sup name={'Cached B'} ms={100000}></Sup></Sup>
+            <DelayedQuery name={'Cached B'} ms={10000}></DelayedQuery>
+            <DelayedQuery name={'Cached B'} ms={4000}><DelayedQuery name={'Cached B'} ms={100000}/></DelayedQuery>
           </Suspense>
         </section>
 
